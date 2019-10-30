b53c25267133fb37f0a5ae182d86181e5ef74b50
Az3r <tuan99kghd@gmail.com>
2019-10-30 19:22:19 +0700

    Update character_device.c

--- cdd/character_device.c
+++ cdd/character_device.c
@@ -107,7 +107,6 @@ static ssize_t my_read(struct file *f, char __user *buf, size_t len, loff_t *off
     {
         // if true then have success
         printk(KERN_INFO "RandomGenerator: Sent %d characters to the user\n", size_of_message);
-        *offset += size_of_message;
         return (size_of_message = 0); // clear the position to the start and return 0
     }
     else
